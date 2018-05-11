d5156a19cebfca3b84ef4414b5c5de0e159502f5
Akraticus <oebl89@gmail.com>
2018-05-11 15:40:36 +0200

    Current local project.

---
+++ LuaConsole/LuaFunctions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuaConsole
+{
+    public class LuaFunctionAttribute : Attribute
+    {
+        public string FunctionName { get; set; }
+
+        public LuaFunctionAttribute(string functionName = null)
+        {
+            FunctionName = functionName;
+        }
+    }
+
+    public static class InternalLuaFunctions
+    {
+        [LuaFunction]
+        public static ILuaResult Test1()
+        {
+            return new LuaResult($"{nameof(Test1)} called.");
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public double Løpehastighet { get; set; } = 10;
+
+        public double MinuttTilMål(double km)
+        {
+            return km / Løpehastighet * 60;
+        }
+    }
+
+    public interface ILuaResult
+    {
+        List<object> Result { get; set; }
+        Exception Exception { get; set; }
+    }
+
+    public class LuaResult : ILuaResult
+    {
+        private List<object> _result = new List<object>();
+        private Exception _exception;
+
+        public List<object> Result { get => _result; set => _result = value; }
+        public Exception Exception { get => _exception; set => _exception = value; }
+
+        public LuaResult(params object[] parameters)
+        {
+            Result = parameters.ToList();
+        }
+    }
+}
---
+++ LuaConsole/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using NLua;
+
+namespace LuaConsole
+{
+	public class Program
+	{
+		private static char separator = ' ';
+
+		static void Main(string[] args)
+		{
+            var lua = new Lua();
+            lua.DoFile("./UserDefinedLuaFunctions.txt");
+
+            var luaMethods = typeof(InternalLuaFunctions).GetMethods().Where(m => m.GetCustomAttributes(typeof(LuaFunctionAttribute), true).Any()).ToList();
+            //var luaMethods = typeof(Program).Assembly.GetTypes().SelectMany(t => t.GetMethods()).Where(m => m.GetCustomAttributes(typeof(LuaMethodAttribute), true).Any()).ToList();
+            foreach (var luaMethod in luaMethods)
+            {
+                var luaFunc = luaMethod.GetCustomAttributes(typeof(LuaFunctionAttribute), true).First() as LuaFunctionAttribute;
+                lua.RegisterFunction(luaFunc.FunctionName ?? luaMethod.Name, luaMethod);
+            }
+
+			while(true)
+			{
+				Console.Write(">> ");
+				var input = Console.ReadLine();
+                var inputSplit = input.Split(' ');
+                var funcName = inputSplit.First();
+
+                // Quit console if
+                if (funcName.ToLowerInvariant().Equals("quit") || funcName.ToLowerInvariant().Equals("exit")) break;
+
+                if (funcName.ToLowerInvariant().Equals("globals"))
+                {
+                    Console.WriteLine($"[{lua.Globals.Count()}] globals.");
+                    foreach (var global in lua.Globals) Console.Write($"{global} ");
+                    Console.WriteLine();
+                    continue;
+                }
+                
+                var func = lua.GetFunction(inputSplit.First());
+                if (func == null)
+                {
+                    Console.WriteLine($"{inputSplit.First()} is not a recognised function.");
+                    continue;
+                }
+
+                try
+                {
+                    var param = inputSplit.Except(new[] { inputSplit.First() });
+                    var result = func.Call(param.ToArray());
+                    Console.WriteLine(result.First());
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+			}
+
+			//Console.WriteLine("Press any key to exit.");
+			//Console.ReadKey(); // stops console from closing immediately
+		}
+	}
+}
